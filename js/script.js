77165ae6fa991360128bf27737c4fa77887448d9
ryoko-y <wagarden6@gmail.com>
2021-07-29 09:53:18 -0400

    add placeholder for each letter

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ const guessedLetter = document.querySelector (".guessed-letters");
 //2.The button with the text “Guess!” in it.
 const button = document.querySelector (".guess");
 //3.The text input where the player will guess a letter.
-const textInput = document.querySelector (".letter");
+const letterInput = document.querySelector (".letter");
 //4.The empty paragraph where the word in progress will appear.
 const wordInProgress = document.querySelector (".word-in-progress");
 //5.The paragraph where the remaining guesses will display.
-const remaining = document.querySelector (".remaining");
+const remainingGuess = document.querySelector (".remaining");
 //6.The span inside the paragraph where the remaining guesses will display.
-const remainingNum = document.querySelector (".remaining span");
+const remainingGuessNum = document.querySelector (".remaining span");
 //7.The empty paragraph where messages will appear when the player guesses a letter.
 const message = document.querySelector (".message");
 //8.The hidden button that will appear prompting the player to play again.
@@ -18,7 +18,13 @@ const playAgain = document.querySelector (".play-again");
 
 const word = "Magnolia";
 
-//Write a Function to Add Placeholders for Each 
-
-
+//Write a Function to Add Placeholders for Each
+const placeholder = fucntion(word) {
+        const placeholderLetters = [];
+        for (const letter of word) {
+          console.log(letter);
+          placeholderLetters.push("●");
+        }
+        wordInProgress.innerText = placeHolderLetters.join("");
+};
 //Add an Event Listener for the Button
